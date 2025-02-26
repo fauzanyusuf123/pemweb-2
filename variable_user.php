efbfecb7aed78b24b3c2ad38f90c0440a89d3fbb
fauzanyusuf123 <fauzanyusuf721@gmail.com>
2025-02-26 12:59:46 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendfinikan variable
+$nama = 'Fauzan' ;
+$umur = 19;
+$berat = 64.64;
+
+echo 'Nama saya ' . $nama;
+echo '<br/>Umur : ' . $umur . ' Tahun ';
+echo '<br/>Berat : ' . $berat;
+
+?>
